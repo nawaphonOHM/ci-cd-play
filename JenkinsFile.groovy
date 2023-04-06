e2b046a60964be03db92355208e0a3b4f77c883a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-04-06 21:31:50 +0700

    add git clone

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -2,11 +2,15 @@
 
 stage('Test') {
     echo "Hello World";
+
+    checkout([$class: 'GitSCM', branches: [[name: '*/main']],
+    userRemoteConfigs: [[url: 'git@github.com:nawaphonOHM/ci-cd-play.git']]])
 }
 
 stage('Test nodejs node') {
     node('nodejs') {
         echo "Hello World from nodejs"
+        
     }
 }
 
---
+++ hello_world.py
@@ -0,0 +1,3 @@
+
+
+print("Hello World inside python script.")
