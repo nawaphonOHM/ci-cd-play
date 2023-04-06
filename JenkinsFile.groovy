41bbf1383a2700c48da5c75af110fb728b7c64f5
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-04-06 16:16:46 +0700

    removed incorrect files

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -3,3 +3,15 @@
 stage('Test') {
     echo "Hello World";
 }
+
+stage('Test nodejs node') {
+    node('nodejs') {
+        echo "Hello World from nodejs"
+    }
+}
+
+stage('Test python3 node') {
+    node('python3') {
+        echo "Hello World form python3"
+    }
+}
