81961fdc70603fb617a61ed77675ecdd29eddc59
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-04-06 16:10:15 +0700

    add python3 node

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,3 +9,9 @@ stage('Test nodejs node') {
         echo "Hello World from nodejs"
     }
 }
+
+stage('Test python3 node') {
+    node('python3') {
+        echo "Hello World form python3"
+    }
+}
