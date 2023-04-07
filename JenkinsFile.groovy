a22071825051656dbe343e00ec63f318d606a0f8
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-04-07 11:54:32 +0700

    add step

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -6,27 +6,34 @@ stage('Test') {
 
 stage('Test nodejs node') {
     node('nodejs') {
-        checkout([
+        step ("Clone Project") {
+            checkout([
                 $class: 'GitSCM', 
                 branches: [[name: '*/main']], 
                 userRemoteConfigs: [[url: 'git@github.com:nawaphonOHM/ci-cd-play.git']]
             ])
+        }
 
-        runNode();
-        // sh(script: "python3 hello_world.py");
+        step ("Run build command") {
+            runNode();
+        }
         
     }
 }
 
 stage('Test python3 node') {
     node('python3') {
-        checkout([
+        step ("Clone Project") {
+            checkout([
                 $class: 'GitSCM', 
                 branches: [[name: '*/main']], 
                 userRemoteConfigs: [[url: 'git@github.com:nawaphonOHM/ci-cd-play.git']]
             ])
-        runPython();
-        // sh(script: "node hello_world.js");
+        }
+
+        step ("Run build command") {
+            runPython();
+        }
     }
 }
 
