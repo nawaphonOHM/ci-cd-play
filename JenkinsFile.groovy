5c7e5b594ef8f982aae0cc64a5c753454e097ab2
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-04-17 10:06:17 +0700

    add pip install

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -4,22 +4,22 @@ stage('Test') {
     echo "Hello World";
 }
 
-stage('Test nodejs node') {
-    node('nodejs') {
-        steps ("Clone Project") {
-            checkout([
-                $class: 'GitSCM', 
-                branches: [[name: '*/main']], 
-                userRemoteConfigs: [[url: 'git@github.com:nawaphonOHM/ci-cd-play.git']]
-            ])
-        }
+// stage('Test nodejs node') {
+//     node('nodejs') {
+//         steps ("Clone Project") {
+//             checkout([
+//                 $class: 'GitSCM', 
+//                 branches: [[name: '*/main']], 
+//                 userRemoteConfigs: [[url: 'git@github.com:nawaphonOHM/ci-cd-play.git']]
+//             ])
+//         }
 
-        steps ("Run build command") {
-            runNode();
-        }
+//         steps ("Run build command") {
+//             runNode();
+//         }
         
-    }
-}
+//     }
+// }
 
 stage('Test python3 node') {
     node('python3') {
@@ -42,5 +42,5 @@ def runNode() {
 }
 
 def runPython() {
-    sh(script: "python3 hello_world.py");
+    sh(script: "pip install --requirement requirements.txt");
 }
